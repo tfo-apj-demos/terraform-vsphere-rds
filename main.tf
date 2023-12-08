b8c2566bb483e1e4ec5198ad2e3c3a28f67c0809
Grant Orchard <go@hashicorp.com>
2023-12-08 15:04:58 +1100

    shrug

--- main.tf
+++ main.tf
@@ -13,38 +13,37 @@ resource "nsxt_policy_ip_address_allocation" "this" {
   pool_path    = data.nsxt_policy_ip_pool.this.path
 }
 
-module "remote_desktop" {
-  source  = "app.terraform.io/tfo-apj-demos/virtual-machine/vsphere"
-  version = "~> 1.3"
+module "rds" {
 
-  num_cpus = 4
-  memory = 8192
+    source  = "app.terraform.io/tfo-apj-demos/virtual-machine/vsphere"
+    version = "~> 1.3"
+    
+    num_cpus = 4
+    memory = 8192
 
-  hostname          = "remote-desktop-server"
-  datacenter        = "Datacenter"
-  cluster           = "cluster"
-  primary_datastore = "vsanDatastore"
-  folder_path       = "management"
-  networks = {
-    "seg-general" : "${nsxt_policy_ip_address_allocation.this.allocation_ip}/22"
-  }
-  dns_server_list = [
-    "172.21.15.150",
-    "10.10.0.8"
-  ]
-  gateway         = "172.21.12.1"
-  dns_suffix_list = ["hashicorp.local"]
-  disk_0_size = 60
+    hostname = "foo"
+    cluster = "cluster"
+    datacenter = "Datacenter"
+    folder_path = "management"
+    primary_datastore = "vsanDatastore"
+    networks = {
+      "seg-general":"${nsxt_policy_ip_address_allocation.this.allocation_ip}/22"
+    }
+    dns_server_list = [
+      "172.21.15.150",
+      "10.10.0.8"
+    ]
+    gateway = "172.21.12.1"
+    dns_suffix_list = ["hashicorp.local"]
+    disk_0_size = 60
 
-  template = data.hcp_packer_image.this.cloud_image_id
-  tags = {}
+    template = data.hcp_packer_image.this.cloud_image_id
 
-  #join_domain = "hashicorp.local"
-  ad_domain = "hashicorp.local"
-  domain_admin_user = "administrator"
-  domain_admin_password = var.domain_admin_password
-  admin_password = var.domain_admin_password
+    ad_domain = "hashicorp.local"
+    domain_admin_user = "administrator"
+    domain_admin_password = var.domain_admin_password
 }
+
 
 module "boundary_target" {
   source  = "app.terraform.io/tfo-apj-demos/target/boundary"
@@ -52,8 +51,8 @@ module "boundary_target" {
 
   hosts = [
     { 
-      "hostname" = module.remote_desktop.virtual_machine_name, 
-      "address" = module.remote_desktop.ip_address
+      "hostname" = module.rds.virtual_machine_name, 
+      "address" = module.rds.ip_address
     }
   ]
   services = [
--- variables.tf
+++ variables.tf
@@ -8,4 +8,5 @@ variable "boundary_token" {
 
 variable "domain_admin_password" {
   type = string
+  default = ""
 }
