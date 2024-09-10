1f072c84145b8cb368633b92cfd7f5f3491aeadb
Aaron Evans <aarone@outlook.com.au>
2024-09-10 17:37:30 +1000

    Refactor module "boundary_target" in main.tf

--- main.tf
+++ main.tf
@@ -77,7 +77,7 @@ resource "vault_token" "this" {
   }
 }
 
-/*module "boundary_target" {
+module "boundary_target" {
   source  = "app.terraform.io/tfo-apj-demos/target/boundary"
   version = "~> 1.4"
 
@@ -97,5 +97,5 @@ resource "vault_token" "this" {
       port             = "3389"
       credential_paths = ["ldap/creds/vault_ldap_dynamic_demo_role"]
     }]
-}*/
+}
 
