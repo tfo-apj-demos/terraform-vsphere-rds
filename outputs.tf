6845b9be71e29cf833e17db872bc14a1611af178
Aaron Evans <aarone@outlook.com.au>
2024-09-19 11:31:38 +1000

    Add generated_aliases_debug output to outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,7 @@ output "virtual_machine_names" {
   value = [for v in module.rds : v.virtual_machine_name]
   
 }
+
+output "generated_aliases_debug" {  
+  value = module.windows_remote_desktop_target.generated_aliases_debug
+}
