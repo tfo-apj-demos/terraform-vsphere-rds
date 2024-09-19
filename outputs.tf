92a81b88db5bd44879174c94cc2b2d075389f899
Aaron Evans <aarone@outlook.com.au>
2024-09-19 12:46:11 +1000

    Remove generated_aliases_debug output from outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -5,7 +5,3 @@ output "virtual_machine_names" {
   value = [for v in module.rds : v.virtual_machine_name]
   
 }
-
-output "generated_aliases_debug" {  
-  value = module.windows_remote_desktop_target.generated_aliases_debug
-}
